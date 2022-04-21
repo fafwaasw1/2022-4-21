ff2fd3fbe28c54e611aa67543746c07c8e038d19
fafwaasw1 <103294037+fafwaasw1@users.noreply.github.com>
2022-04-21 10:30:52 +0800

    Add files via upload

---
+++ 01.Thead/01.demo/CPU.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.VisualBasic.Devices;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01.demo
+{
+    public class CPU
+    {
+        /// <summary>
+        /// cpu
+        /// </summary>
+        /// <returns></returns>
+        public string ServiceDemo()
+        {
+            //CPU利用率
+            float fCPU = GetCpu();
+
+            return fCPU.ToString();
+        }
+
+        /// <summary>
+        /// 内存
+        /// </summary>
+        /// <returns></returns>
+        public string ServiceDemo1()
+        {
+            //获得内存
+            ulong physicalMey;
+            ulong totalMey;
+            ulong rMey;
+
+            double dMermory = GetMemory(out physicalMey, out totalMey, out rMey);
+
+            return dMermory.ToString();
+        }
+
+
+        /// <summary>
+        /// 硬盘总容量
+        /// </summary>
+        /// <returns></returns>
+        public string ServiceDemo2()
+        {
+            //硬盘采集
+            long lDisk = GetHardDiskSpace("c");
+            
+
+            return lDisk.ToString();
+
+        }
+
+        /// <summary>
+        /// 硬盘剩余容量
+        /// </summary>
+        /// <returns></returns>
+
+        public string ServiceDemo3()
+        {
+            long rDisk = GetHardDiskFreeSpace("c");
+            return rDisk.ToString();
+        }
+
+
+
+
+        /// <summary>
+        /// 获取CPU
+        /// </summary>
+        public static float GetCpu()
+        {
+            PerformanceCounter pcCPU = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            float f = pcCPU.NextValue();
+            Thread.Sleep(1000);
+            f = pcCPU.NextValue();
+            string s1 = f.ToString("0.00");
+            return f = float.Parse(s1);
+        }
+
+
+        /// <summary>
+        /// 获取内存利用率
+        /// </summary>
+        /// <param name="physicalMey"></param>
+        /// <param name="totalMey"></param>
+        /// <param name="rMey"></param>
+        public static double GetMemory(out ulong physicalMey, out ulong totalMey, out ulong rMey)
+        {
+            //内存
+            ComputerInfo c1 = new ComputerInfo();
+
+            //可用内存
+            physicalMey = c1.AvailablePhysicalMemory;
+            //总内存
+            totalMey = c1.TotalPhysicalMemory;
+            //已用内存
+            rMey = totalMey - physicalMey;
+
+
+            //获得内存占有率
+            double a = (double)totalMey;
+            double b = (double)rMey;
+
+
+            string s = (rMey / totalMey).ToString("0.00");
+
+            double t = double.Parse((b / a).ToString("0.000"));
+            return t = t * 100;
+
+        }
+
+
+
+
+        /// <summary>
+        /// 读取剩余硬盘容量
+        /// </summary>
+        /// <param name="str_HardDiskName"></param>
+        /// <returns></returns>
+        public static long GetHardDiskFreeSpace(string str_HardDiskName)
+        {
+            long freeSpace = new long();
+            str_HardDiskName = (str_HardDiskName + ":\\").ToUpper();
+            System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+            foreach (System.IO.DriveInfo drive in drives)
+            {
+                if (drive.Name == str_HardDiskName)
+                {
+                    freeSpace = drive.TotalFreeSpace / (1024 * 1024 * 1024);
+                }
+            }
+            return freeSpace;
+        }
+
+
+
+        /// <summary>
+        /// 读取硬盘容量
+        /// </summary>
+        /// <param name="str_HardDiskName"></param>
+        /// <returns></returns>
+        public static long GetHardDiskSpace(string str_HardDiskName)
+        {
+            long totalSize = new long();
+            str_HardDiskName = str_HardDiskName + ":\\";
+            str_HardDiskName = str_HardDiskName.ToUpper();
+            System.IO.DriveInfo[] drives = System.IO.DriveInfo.GetDrives();
+            foreach (System.IO.DriveInfo drive in drives)
+            {
+                if (drive.Name == str_HardDiskName)
+                {
+                    totalSize = drive.TotalSize / (1024 * 1024 * 1024);
+                }
+            }
+            return totalSize;
+        }
+
+
+
+    }
+}
---
+++ 01.Thead/01.demo/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _01.demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        Thread th;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           
+            //创建一个线程去执行的方法
+            th = new Thread(Test);
+
+            th.Start();
+
+        }
+
+        Thread th1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(Test1);
+            th1.Start();
+        }
+
+        Thread th2;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            th2 = new Thread(Test2);
+            th2.Start();
+        }
+
+
+        private void Test()
+        {
+            CPU cp = new CPU();
+            for (int i = 0; i < 100000; i++)
+            {
+                textBox1.Text = cp.ServiceDemo()+"%";
+            }
+
+        }
+
+        private void Test1()
+        {
+            CPU cp = new CPU();
+            for (int i = 0; i < 10000; i++)
+            {
+                textBox2.Text = cp.ServiceDemo1() + "MB";
+            }
+        }
+
+
+        private void Test2()
+        {
+            CPU cp = new CPU();
+            for (int i = 0; i < 10000; i++)
+            {
+                textBox3.Text = "硬盘总容量："+ cp.ServiceDemo2() + "MB,硬盘剩余容量：" + cp.ServiceDemo3() +"MB" ;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //取消跨线程的访问
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //关闭窗体，判断线程是否为null
+            if(th!=null )
+            {
+                //立即结束线程
+                th.Abort();
+            }
+            if(th1!=null)
+            {
+                th1.Abort();
+            }
+            if (th2 != null)
+            {
+                th2.Abort();
+            }
+
+        }
+
+      
+    }
+}
